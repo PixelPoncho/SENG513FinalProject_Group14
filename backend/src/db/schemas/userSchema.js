ace7f3b158c0167a950563e28706c080bc36f6d4
Kiernan McGuigan <46582079+kmcguigan1@users.noreply.github.com>
2022-04-05 19:55:12 -0600

    changed data formatting

--- backend/src/db/schemas/userSchema.js
+++ backend/src/db/schemas/userSchema.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: String,
     username: String,
+    email: String,
     password: String,
 
     avatar: {
