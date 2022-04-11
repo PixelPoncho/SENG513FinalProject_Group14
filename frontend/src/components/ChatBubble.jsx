f7e66c1ab68224231b9d313f85ac045e22de7e8f
Melissa <57367085+melpicazo@users.noreply.github.com>
2022-04-10 23:38:14 -0600

    add chat bubble

---
+++ frontend/src/components/ChatBubble.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react'
+
+import '../styles/ChatBubble.scss';
+
+const ChatBubble = props => {
+    const {
+        
+    } = props;
+
+    
+
+    return (
+        <div className="chat-bubble">
+        
+        </div>
+    );
+}
+
+export default ChatBubble;
